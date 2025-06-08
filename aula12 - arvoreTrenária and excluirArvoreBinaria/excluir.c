e66586c3d9a8ff2f340377dfad3831babfda3c2e
Cleber Henrique L Duarte <141072429+cleberhdev@users.noreply.github.com>
2025-06-08 20:58:21 -0300

    featura/aulas_12

--- aula12 - arvoreTrenária and excluirArvoreBinaria/excluir.c
+++ aula12 - arvoreTrenária and excluirArvoreBinaria/excluir.c
@@ -33,25 +33,3 @@ int buscar(Arv *a, char c){
         //se a primeira operação for verdadeira, ele já retorna, por isso que é bom colocar em ordem a expressão que tem a maior a probabilidade de ser verdadeira;
     }
 }
-//obs: implemente depois
-//obs: olhe o método exibir para ver sobre recursividade
-
-int main() {
-    // Criando nós folha
-    Arv *d = criar('D', NULL, NULL);
-    Arv *e = criar('E', NULL, NULL);
-    Arv *f = criar('F', NULL, NULL);
-
-    // Criando nós intermediários
-    Arv *b = criar('B', NULL, d);
-    Arv *c = criar('C', e, f);
-
-    // Criando raiz
-    Arv *raiz = criar('A', b, c);
-
-    // Exibindo a árvore
-    printf("Arvore: ");
-    exibir(raiz);
-    printf("\n");
-    return 0;
-}
